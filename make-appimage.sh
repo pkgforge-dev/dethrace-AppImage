1a7848ff0c21ffe76062445e536a425591e73c8f
Link4Electronics <link4electronics@gmail.com>
2026-04-11 17:56:07 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,15 +5,21 @@ set -eu
 ARCH=$(uname -m)
 export ARCH 
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export STARTUPWMCLASS=dethrace
+#export STARTUPWMCLASS=dethrace
 export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/dethrace
 
-# Additional changes can be done in between here
+# this app has problems with other locales breaking physics
+echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
