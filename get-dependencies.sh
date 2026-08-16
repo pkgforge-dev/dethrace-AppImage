ae4685efeb6ac66b2e57cee714835c4f740e782e
Link4Electronics <link4electronics@gmail.com>
2026-08-15 21:27:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,10 +17,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Building dethrace..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/Link4Electronics/dethrace"
