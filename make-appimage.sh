6ac6a4ae1aa77bc8db8fd123fe15260661affae0
Link4Electronics <link4electronics@gmail.com>
2026-08-16 11:54:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,5 +14,8 @@ export DEPLOY_VULKAN=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/dethrace
 
+# this app has problems with other locales breaking physics
+echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
+
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
