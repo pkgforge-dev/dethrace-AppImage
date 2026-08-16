509716906dfaaa69db2290e968cc00179f718fc3
Link4Electronics <link4electronics@gmail.com>
2026-08-16 10:06:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export STARTUPWMCLASS=dethrace
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/dethrace
