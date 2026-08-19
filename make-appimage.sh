bf91259180b75ef1d460199cf20f7a406d0bde3f
Link4Electronics <link4electronics@gmail.com>
2026-08-19 13:35:26 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export STARTUPWMCLASS=dethrace
+#export STARTUPWMCLASS=dethrace
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
